dc11d61adfcf087467fa752a3ba73a9ee00a99ba
keentooyyy <kenneth.lobaton.58@gmail.com>
2025-12-13 23:48:07 +0800

    login and register UI

--- SaveNLoad/urls.py
+++ SaveNLoad/urls.py
@@ -5,5 +5,6 @@ app_name = 'SaveNLoad'
 
 urlpatterns = [
     path('', views.route_login, name='route_login'),
+    path('register/', views.route_register, name='route_register'),
 ]
 
--- SaveNLoad/views.py
+++ SaveNLoad/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def route_login(request):
     """Front-facing login/home page"""
     return render(request, 'SaveNLoad/login.html')
+
+def route_register(request):
+    """Registration page"""
+    return render(request, 'SaveNLoad/register.html')
